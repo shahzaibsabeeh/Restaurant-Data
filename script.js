86eba80b6d302776895daf9e74bf90b90a581b92
shahzaibsabeeh <135643807+shahzaibsabeeh@users.noreply.github.com>
2023-06-06 11:10:54 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 // script.js
 
 document.getElementById("customerForm").addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevent the form from submitting normally
+  event.preventDefault();
 
-  // Get form values
   const name = document.getElementById("name").value;
   const email = document.getElementById("email").value;
   const phone = document.getElementById("phone").value;
   const item = document.getElementById("item").value;
   const quantity = document.getElementById("quantity").value;
 
-  // Perform API request or further processing here
-  // You can use libraries like Axios or fetch to make API requests to your back-end server
-  // For example:
-  // axios.post("/api/invoices", { name, email, phone, item, quantity })
-  //   .then(function(response) {
-  //     // Handle successful response
-  //   })
-  //   .catch(function(error)
+  // Make an AJAX request
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "generate_invoice.php", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      // Handle the response
+      const invoiceHTML = xhr.responseText;
+      // Display the invoice HTML on another page or dynamically inject it into the current page
+      // For example, you can redirect to another page and pass the invoice HTML as a query parameter
+      window.location.href = "invoice.html?invoice=" + encodeURIComponent(invoiceHTML);
+    }
+  };
+  const data = "name=" + encodeURIComponent(name) + "&email=" + encodeURIComponent(email) + "&phone=" + encodeURIComponent(phone) + "&item=" + encodeURIComponent(item) + "&quantity=" + encodeURIComponent(quantity);
+  xhr.send(data);
+});
