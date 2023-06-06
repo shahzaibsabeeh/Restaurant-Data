c54d24d42aba9f8afd501cd552d02373f9ca0ed0
shahzaibsabeeh <135643807+shahzaibsabeeh@users.noreply.github.com>
2023-06-06 12:20:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,46 @@
-// script.js
-
 document.getElementById("customerForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const phone = document.getElementById("phone").value;
-  const item = document.getElementById("item").value;
-  const quantity = document.getElementById("quantity").value;
-
-  // Make an AJAX request
-  const xhr = new XMLHttpRequest();
-  xhr.open("POST", "generate_invoice.php", true);
-  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-      // Handle the response
-      const invoiceHTML = xhr.responseText;
-      // Display the invoice HTML on another page or dynamically inject it into the current page
-      // For example, you can redirect to another page and pass the invoice HTML as a query parameter
-      window.location.href = "invoice.html?invoice=" + encodeURIComponent(invoiceHTML);
-    }
-  };
-  const data = "name=" + encodeURIComponent(name) + "&email=" + encodeURIComponent(email) + "&phone=" + encodeURIComponent(phone) + "&item=" + encodeURIComponent(item) + "&quantity=" + encodeURIComponent(quantity);
-  xhr.send(data);
-});
+    event.preventDefault();
+  
+    // Get customer input values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var item = document.getElementById("item").value;
+    var quantity = document.getElementById("quantity").value;
+  
+    // Calculate totals
+    var price = 10; // Assuming a constant price of $10 per item
+    var subtotal = price * quantity;
+    var tax = 0.1 * subtotal;
+    var total = subtotal + tax;
+  
+    // Populate invoice table
+    var invoiceTable = document.getElementById("invoiceItems");
+    invoiceTable.innerHTML = "";
+    var row = document.createElement("tr");
+    var itemCell = document.createElement("td");
+    itemCell.textContent = item;
+    row.appendChild(itemCell);
+    var quantityCell = document.createElement("td");
+    quantityCell.textContent = quantity;
+    row.appendChild(quantityCell);
+    var priceCell = document.createElement("td");
+    priceCell.textContent = "$" + price.toFixed(2);
+    row.appendChild(priceCell);
+    var totalCell = document.createElement("td");
+    totalCell.textContent = "$" + subtotal.toFixed(2);
+    row.appendChild(totalCell);
+    invoiceTable.appendChild(row);
+  
+    // Update totals
+    document.getElementById("subtotal").textContent = "$" + subtotal.toFixed(2);
+    document.getElementById("tax").textContent = "$" + tax.toFixed(2);
+    document.getElementById("total").textContent = "$" + total.toFixed(2);
+  
+    // Show invoice container
+    document.getElementById("invoiceContainer").style.display = "block";
+  });
+  
+  
+  
+  
